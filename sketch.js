718cf7a33e3609498ee1667c9d3be07344a2efee
Snow-Sun-Tiger <emma.hein@tellkampfschule.eu>
2024-09-23 14:15:47 +0200

    Update

--- sketch.js
+++ sketch.js
@@ -13,15 +13,19 @@ let yMovement = -Y_SPEED;
 
 
 class Platform {
-  constructor(x, y) {
+  constructor(x, y, color) {
     this.x = x;
     this.y = y;
+    this.color = color;
   }
 }
 let canvasWidth;
 let canvasHeight;
 
- let platforms = [];
+let platforms = [];
+
+let highscore = 0;
+
 
 // Hintergrund width, height 
 function setup() {
@@ -30,35 +34,41 @@ function setup() {
   createCanvas(canvasWidth, canvasHeight);
 }
 
-let platformY = 800
+let platformY = 800;
+let punktestand = 0;
 
 //Helligkeit Dunkel < Hell 
-function draw() { 
+function draw() {
   background(10);
 
   if (platforms.length == 0) {
-    platforms = [new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight)),
-    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight)),
-    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight)),
-    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight)),
-    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight)),
-    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight)),
-    new Platform(canvasWidth / 2 , canvasHeight - 200)];
+    platforms = [new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight),"yellow"),
+    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight),'red'),
+    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight),'blue'),
+    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight),'green'),
+    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight),"red"),
+    new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight),"red"),
+    new Platform(canvasWidth / 2, canvasHeight - 200, 'lightgreen')];
   }
 
   for (let i = 0; i < platforms.length; i++) {
     let isPlayerHittingOnX = platforms[i].x - 25 < xPlayer && xPlayer < platforms[i].x + 80
-    let isPlayerHittingOnY = platforms[i].y - 30 < yPlayer &&  yPlayer - 5 < platforms[i].y
+    let isPlayerHittingOnY = platforms[i].y - 30 < yPlayer && yPlayer - 5 < platforms[i].y
+    
 
     if (isPlayerHittingOnX && isPlayerHittingOnY && yMovement > 0) {
       console.log("Spieler auf Plattform")
+      punktestand = punktestand + 1;
+      console.log("Punktestand: " + punktestand)
+
       yMovement = -Y_SPEED
       platformY = platforms[i].y
-      platforms[i] = new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight));
+      let oldColor = platforms[i].color;
+      platforms[i] = new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight), oldColor)
     }
   }
 
-  
+
   yPlayer = yPlayer + yMovement
 
   //lässt den Spieler runterkommen
@@ -76,33 +86,49 @@ function draw() {
   }
 
 
-  if (keyIsDown(ENTER) || yPlayer > canvasHeight + 100) {
+  if (keyIsDown(ENTER) || yPlayer > canvasHeight + 200) {
     xPlayer = canvasWidth / 2
-
     // ist die Position des Spielers auf der y-Achse (obere linke Ecke)  
     yPlayer = 10;
+    if (punktestand > highscore) {
+      highscore = punktestand;
+    }
+    punktestand = 0;
+    for (let i = 0; i < platforms.length; i++) {
+        let oldColor = platforms[i].color;
+        platforms[i] = new Platform(Math.floor(Math.random() * canvasWidth), Math.floor(Math.random() * canvasHeight), oldColor)
+    }
   }
-
-
-  if (touches.length > 0 && touches[0].x > 700/2) {
+  if (touches.length > 0 && touches[0].x > displayWidth / 2) {
     xPlayer = xPlayer + 5
   }
 
-  if (touches.length > 0 && touches[0].x < 700/2) {
+  if (touches.length > 0 && touches[0].x < displayWidth / 2) {
     xPlayer = xPlayer - 5
+
+   
+
   }
   
+  
 
-// Plattformen einzeichnen
-for (let i = 0; i < platforms.length; i++) {
-  rect(platforms[i].x, platforms[i].y, 80, 15)
-}
+  // Plattformen einzeichnen
+  for (let i = 0; i < platforms.length; i++) {
+    fill(platforms[i].color)
+    rect(platforms[i].x, platforms[i].y, 80, 15)
 
-
-// Spieler zeichnen
-square(xPlayer, yPlayer, 25)
-
-
+    fill('blue')
+  }
 
 
+  // Spieler zeichnen
+  fill('lightblue')
+  square(xPlayer, yPlayer, 25, 4,)
+  
+
+  textSize(22)
+  fill("yellow")
+  text('score ' + punktestand, canvasWidth / 1.2, canvasHeight / 18, 50)
+  text('highscore ' + highscore, canvasWidth / 1.2, canvasHeight / 18 + 60, 50)
+  fill("white")
 }
